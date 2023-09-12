fb198893b73d738815e09d30ed298d59a1742a17
GiftedNova <72659439+GiftedNovaHD@users.noreply.github.com>
2023-09-12 13:12:44 +0800

    I screwed up my commit shit
    :skull: backup ig

---
+++ README.md
@@ -0,0 +1,57 @@
+# AI Image Object Detection Web Tool
+
+This repository contains a web-based tool for AI image object detection built upon the **DEtection TRansformer (DETR)** model. Our approach views object detection as a direct set prediction problem, effectively streamlining the detection pipeline. This tool leverages the power of DETR to perform object detection on images provided by users.
+
+## Table of Contents
+- [Introduction](#introduction)
+- [Features](#features)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [DETR model](#detr-model)
+- [Web Interface](#web-interface)
+
+
+## Introduction
+Our web-based AI image object detection tool is designed to simplify the process of detecting objects in images. It utilizes the DETR model, which treats object detection as a set prediction problem, eliminating the need for many hand-designed components. This tool provides an intuitive and user-friendly interface for users to upload images and receive object detection results.
+
+## Features
+- **Object Detection**: The tool uses the DETR model to detect objects within images, providing bounding box predictions and class labels for each object.
+- **Web Interface**: A user-friendly web interface allows users to upload images for object detection and view the results directly on the webpage.
+- **DETR Model**: The DETR model, as described in the research paper, is used as the underlying AI model for object detection. It provides accurate and efficient results.
+- **Real-time Inference**: The tool performs real-time inference, making it suitable for a wide range of applications, including image analysis, content moderation, and more.
+
+## Getting Started
+
+### Prerequisites
+Before running the web tool, ensure you have the following prerequisites installed:
+- Any version of Python >= 3.8
+- `pip` package manager
+- Virtual environment (optional but recommended)
+- NVIDIA GPU with CUDA support (highly recommended for inference)
+- PyTorch
+
+### Installation
+1. Navigate to `main.py`. 
+
+## DETR Model
+Our tool utilizes the DEtection TRansformer (DETR) model, as described in the research paper. DETR represents a breakthrough in object detection by treating it as a set prediction problem, eliminating the need for many hand-designed components. The model can be easily generalized for various object detection tasks and provides state-of-the-art performance.
+
+For more details on the DETR model and its implementation, refer to the official [DETR GitHub repository](https://github.com/facebookresearch/detr).
+
+## Web Interface
+The web interface of our tool allows users to interact with the DETR model seamlessly. Here are the main components of the web interface:
+
+- **Upload Image**: Users can upload an image directly from their local device.
+- **Object Detection**: Once the image is uploaded, the tool performs object detection using DETR and displays the results, including bounding boxes around detected objects and their corresponding class labels.
+- **Customization**: Users can adjust detection parameters, such as confidence threshold, to fine-tune the detection results.
+- **Real-time inference**: The tool provides real-time inference, allowing users to see the detection results instantly.
+
+# License
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,120 @@
+# Penis
+import torch
+from transformers import DetrImageProcessor, DetrForObjectDetection
+from PIL import Image
+from flask import Flask, render_template, request
+import sqlite3
+import os
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/classify", methods=["POST"])
+def infer() -> str | None:
+  try:
+    image_file = request.files.get("image", "")
+    image_path = os.path.join(".\\static\\images", image_file.filename)
+    image_file.save(image_path)
+    image_path = f"images/{image_file.filename}"
+    upload_date = datetime.datetime.now()
+
+    image = Image.open(image_file)
+  except Exception as e:
+    return render_template("error.html", error=e)
+
+  inputs = processor(images=image, return_tensors="pt")
+  inputs = inputs.to(device) 
+  outputs = model(**inputs)
+
+  target_sizes = torch.tensor([image.size[::-1]]).to(device) # 
+  results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+
+  detection_info = []
+  for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+    box = [round(i, 2) for i in box.tolist()]
+    class_name = model.config.id2label[label.item()]
+    detection_info.append({
+      "class_name": class_name,
+      "confidence": round(score.item(), 3),
+      "location": box
+        })
+
+  try:
+    conn = sqlite3.connect("image_classification.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                   INSERT INTO Images (ImageName, ImageFile, UploadDate) 
+                   VALUES (?,?,?)
+    """, (image_file.filename, image_path, upload_date))
+
+  except sqlite3.Error as e: 
+    return render_template('error.html', error=e)
+  finally:
+    conn.close()
+  
+  return render_template('index.html', result=detection_info, image_url=f"images/{image_file.filename}") 
+
+@app.route("/")
+def homepage():
+  return render_template("index.html")
+
+@app.route("/images")
+def view_images():
+  try: 
+    conn = sqlite3.connect('image_classification.db')
+    cursor = conn.cursor()
+  
+    cursor.execute("SELECT ImageID, ImageName, ImageFile, UploadDate FROM Images")
+
+    image_data = []
+    for row in cursor.fetchall():
+      image_data.append({
+      "ImageID": row[0],
+      "ImageName": row[1],
+      "ImageFile": row[2],
+      "UploadDate": row[3]
+      })
+  
+    return render_template('image.html', image_data=image_data) #fuck jinja 
+  except sqlite3.Error as e: 
+    print(f"Database error: {e}")
+    return []
+  
+  finally: 
+    conn.close()
+
+@app.route("/image")
+def show_image():
+  image_id = request.args.get("id")
+
+  if image_id is None:
+    return render_template("error.html", error="No id parameter supplied you bozo")
+  
+  try:
+    conn = sqlite3.connect('image_classification.db')
+    cursor = conn.cursor()
+  
+    cursor.execute(f"SELECT * FROM Images WHERE ImageID = {image_id}")
+    image = cursor.fetchall()[0]
+  
+  except Exception as e:
+    return render_template("error.html", error=e)
+
+  finally:
+    conn.close()
+
+  return render_template("image.html", image=image)
+
+@app.route("/about")
+def about_the_architecture(): 
+  return render_template("about.html")
+
+if __name__ == "__main__": 
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+  processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-101")
+  model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-101")
+  model = model.to(device)
+
+  app.run(debug=True)
