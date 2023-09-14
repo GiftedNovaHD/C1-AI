88da5f0eb0db6a4ebed5b9b2d0c53d7fd20e7f8a
GiftedNova <quantum.atomicsplitters@gmail.com>
2023-09-15 00:24:58 +0800

    cock department

--- junk/create-database.py
+++ junk/create-database.py
@@ -7,25 +7,36 @@ cursor = conn.cursor()
 # Create the Images table
 cursor.execute('''
   CREATE TABLE IF NOT EXISTS Images (
-    ImageID INTEGER PRIMARY KEY AUTOINCREMENT,
-    ImageName TEXT NOT NULL,
-    ImageFile TEXT NOT NULL,
-    UploadDate DATETIME NOT NULL
-    )
+  ImageID INTEGER PRIMARY KEY AUTOINCREMENT,
+  ImageName TEXT NOT NULL,
+  ImageFile TEXT NOT NULL,
+  UploadDate DATETIME NOT NULL
+  )
+''')
+
+# Create the Classes table
+cursor.execute('''
+  CREATE TABLE IF NOT EXISTS Classes (
+  ClassID INTEGER PRIMARY KEY AUTOINCREMENT,
+  ClassName TEXT NOT NULL
+  )
 ''')
 
 # Create the Detections table
 cursor.execute('''
   CREATE TABLE IF NOT EXISTS Detections (
-    DetectionID INTEGER PRIMARY KEY AUTOINCREMENT,
-    ImageID INTEGER NOT NULL,
-    Class TEXT NOT NULL,
-    Confidence REAL NOT NULL,
-    Location TEXT NOT NULL,
-    FOREIGN KEY (ImageID) REFERENCES Images (ImageID)
-    )
+  DetectionID INTEGER PRIMARY KEY AUTOINCREMENT,
+  ImageID INTEGER NOT NULL,
+  ClassID INTEGER NOT NULL,
+  Confidence REAL NOT NULL,
+  Location TEXT NOT NULL,
+  FOREIGN KEY (ImageID) REFERENCES Images (ImageID),
+  FOREIGN KEY (ClassID) REFERENCES Classes (ClassID)
+  )
 ''')
 
 # Commit the changes and close the connection
 conn.commit()
 conn.close()
+
+print("Database setup completed.")
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import torch
-from transformers import DetrImageProcessor, DetrForObjectDetection
-from PIL import Image
-from flask import Flask, render_template, request, redirect
 import sqlite3
 import os
 import datetime
+import json
+
+from transformers import DetrImageProcessor, DetrForObjectDetection
+from PIL import Image
+from flask import Flask, render_template, request, redirect
+
 
 app = Flask(__name__)
 
@@ -13,6 +16,11 @@ def infer() -> str | None:
   try:
     image_file = request.files.get("image", "")
     image_path = os.path.join(".\\static\\images", image_file.filename)
+
+    if os.path.exists(image_path):
+      return render_template("index.html")
+
+    # so that there's no duplicate image 
     image_file.save(image_path)
     image_path = f"images/{image_file.filename}"
     upload_date = datetime.datetime.now()
@@ -36,7 +44,7 @@ def infer() -> str | None:
       "class_name": class_name,
       "confidence": round(score.item(), 3),
       "location": box
-        })
+      })
 
   try:
     conn = sqlite3.connect("image_classification.db", isolation_level=None)
@@ -46,6 +54,29 @@ def infer() -> str | None:
                    INSERT INTO Images (ImageName, ImageFile, UploadDate) 
                    VALUES (?,?,?)
     """, (image_file.filename, image_path, upload_date))
+
+    image_id = cursor.lastrowid
+
+    for info in detection_info:
+      class_name = info["class_name"]
+      confidence = info["confidence"]
+      location = json.dumps(info["location"])
+
+      # Retrieve the ClassID for the class name or insert it if it doesn't exist
+      cursor.execute("SELECT ClassID FROM Classes WHERE ClassName = ?", (class_name,))
+      class_id = cursor.fetchone()
+      if class_id is None:
+        cursor.execute("INSERT INTO Classes (ClassName) VALUES (?)", (class_name,))
+        class_id = cursor.lastrowid
+      else:
+        class_id = class_id[0]
+
+        # Insert detection data
+      cursor.execute(
+        """
+        INSERT INTO Detections (ImageID, ClassID, Confidence, Location) 
+        VALUES (?,?,?,?)
+        """, (image_id, class_id, confidence, location))
 
   except sqlite3.Error as e:
     conn.close()
@@ -94,8 +125,15 @@ def show_image():
     conn = sqlite3.connect('image_classification.db')
     cursor = conn.cursor()
   
-    cursor.execute(f"SELECT * FROM Images WHERE ImageID = {image_id}")
+    cursor.execute(f"SELECT * FROM Images WHERE ImageID = ? ", (image_id))
     image = cursor.fetchall()[0]
+
+    image_data = {
+      "ImageID": image[0],
+      "ImageName": image[1],
+      "ImageFile": image[2],
+      "UploadDate": image[3]
+    }
   
   except Exception as e:
     conn.close()
@@ -104,7 +142,56 @@ def show_image():
   finally:
     conn.close()
 
-  return render_template("image.html", image=image)
+  return render_template("image.html", image=image_data)
+
+@app.route("/classes")
+def classes():
+  try:
+    conn = sqlite3.connect('image_classification.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+      SELECT
+        C.ClassName,
+        I.ImageName,
+        I.ImageFile,
+        I.UploadDate,
+        I.ImageID
+      FROM Classes AS C
+      LEFT JOIN
+        Detections AS D ON C.ClassID = D.ClassID
+      LEFT JOIN
+        Images AS I ON D.ImageID = I.ImageID
+      """)
+    data = cursor.fetchall() # tuple 
+
+    classes_data = {}
+    for d in data:
+      if d[2] is None:
+        continue
+      if d[0] in classes_data.keys():
+        classes_data[d[0]].append({
+          "ImageName": d[1],
+          "ImageFile": d[2],
+          "UploadDate": d[3],
+          "ImageID": d[4]
+        })
+      else:
+        classes_data[d[0]] = [
+        {
+          "ImageName": d[1],
+          "ImageFile": d[2],
+          "UploadDate": d[3],
+          "ImageID": d[4]
+        }
+      ]
+
+      
+    return render_template("classes.html", data=classes_data)
+    
+  except Exception as e:
+    conn.close()
+    return render_template("error.html", error=e)
 
 @app.route("/about")
 def about_the_architecture(): 
@@ -152,6 +239,7 @@ def delete_image():
     image_path = cursor.fetchone()[0]
 
     cursor.execute("DELETE FROM Images WHERE ImageID = ?", (image_id))
+    cursor.execute("DELETE FROM Detections WHERE ImageID = ?", (image_id))
     conn.commit()
 
     os.remove("./static/" + image_path)
@@ -172,4 +260,4 @@ if __name__ == "__main__":
   model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-101")
   model = model.to(device)
 
-  app.run(debug=True)
+  app.run(debug=False)
